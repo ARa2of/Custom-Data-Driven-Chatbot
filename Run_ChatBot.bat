0b9d2c327e2b6795c75163885f2771015861f98e
Ahmed A.Raouf Mohamed <ra2ooof@gmail.com>
2025-02-28 23:52:02 +0000

    Update Run_ChatBot.bat

--- Run_ChatBot.bat
+++ Run_ChatBot.bat
@@ -1,4 +1,4 @@
 @echo off
-"C:\Users\Ahmad\miniconda3\python.exe" "D:/WORK/Python Projects/Custom Data-Driven Chatbot/chatbot.py"
+"C:\Users\XYZ\miniconda3\python.exe" "D:/Directory/chatbot.py"
 timeout /t 5 /nobreak >nul
 start http://127.0.0.1:7860/
