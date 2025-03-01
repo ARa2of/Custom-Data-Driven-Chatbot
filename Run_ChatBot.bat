9eb6636514537a7ca35597a01829a579bc4c39e9
Ahmed A.Raouf Mohamed <69669859+ARa2of@users.noreply.github.com>
2025-03-01 00:07:42 +0000

    Update Run_ChatBot.bat

--- Run_ChatBot.bat
+++ Run_ChatBot.bat
@@ -1,4 +1,5 @@
 @echo off
-"C:\Users\XYZ\miniconda3\python.exe" "D:/Directory/chatbot.py"
+set BAT_DIR=%~dp0
+"C:\Users\XYZ\miniconda3\python.exe" "D:/Directory/chatbot.py" "%BAT_DIR%chatbot.py"
 timeout /t 5 /nobreak >nul
 start http://127.0.0.1:7860/
